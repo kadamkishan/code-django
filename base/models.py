293c4f2bd6568299e0274275e5ac6cb5dfd420e1
Kishan Kadam <76566420+kadamkishan@users.noreply.github.com>
2022-03-29 16:16:47 +0530

    update models.py
    here indentation wrong which is line no 36

--- base/models.py
+++ base/models.py
@@ -33,6 +33,6 @@ class Message(models.Model):
     updated=models.DateTimeField(auto_now=True)
     created=models.DateTimeField(auto_now_add=True)
 
-def __str__(self):
-    return self.body[0:50]
+    def __str__(self):
+        return self.body[0:50]
 
